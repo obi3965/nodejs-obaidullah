326830aa8cca759cc769a502d4d70dff7e707a86
obaidullah3965 <33298720+obaidullah3965@users.noreply.github.com>
2019-09-12 23:15:07 +0200

    commit

---
+++ helper/database.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+
+
+const pool = mysql.createPool({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'node-complete'
+})
+
+module.exports = pool.promise();
