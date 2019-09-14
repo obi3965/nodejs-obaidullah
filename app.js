cfe13dd76923957b926f95e4b8f1f366b9bbdc88
obaidullah3965 <33298720+obaidullah3965@users.noreply.github.com>
2019-09-14 14:02:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 
-const db = require('./helper/database')
+const sequelize = require('./helper/database')
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -24,4 +24,12 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize.sync().then(result =>{
+    // console.log(result);
+    app.listen(5010);
+})
+
+.catch(err =>{
+    console.log(err)
+})
+
--- helper/database.js
+++ helper/database.js
@@ -1,11 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-
-const pool = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'node-complete'
+const sequelize = new Sequelize('node-complete', 'root', '', {
+    dialect:'mysql',
+    host:'localhost'
 })
 
-module.exports = pool.promise();
+module.exports = sequelize;
